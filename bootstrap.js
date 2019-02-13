64b79baaa422d536d0faa35ac2a197244da2b816
John Bieling <john.bieling@gmx.de>
2019-02-13 21:09:46 +0100

    beta installer is now an independent addon

--- bootstrap.js
+++ bootstrap.js
@@ -11,28 +11,28 @@ function installAddOn (url, name ) {
             aAddonInstall.install();
             resolve();
         }
-
         AddonManager.getInstallForURL(url, doSilentInstall, "application/x-xpinstall", null, name);
     });
 }
 
-function setDisableState (enableState) {
+function getAddOn (id) {
     return new Promise(function(resolve, reject) {
-        let finalizeInitByWaitingForAddons = function (addons) {
-            for (let a=0; a < addons.length; a++) {
-                switch (addons[a].id.toString()) {
-                case "eas4tbsync@jobisoft.de":
-                    addons[a].userDisabled = enableState;
-                    break;
-                case "dav4tbsync@jobisoft.de":
-                    addons[a].userDisabled = enableState;
-                    break;
-                }
-            }
-            resolve();
+        let doGetAddon = function (addon) {
+            resolve(addon);
         }
-        
-        AddonManager.getAllAddons(finalizeInitByWaitingForAddons);
+        AddonManager.getAddonByID(id, doGetAddon);
+    });
+}
+
+function sleep(delay) {
+    let timer =  Components.classes["@mozilla.org/timer;1"].createInstance(Components.interfaces.nsITimer);        
+    return new Promise(function(resolve, reject) {
+        let event = {
+            notify: function(timer) {
+                    resolve();
+            }
+        }            
+        timer.initWithCallback(event, delay, Components.interfaces.nsITimer.TYPE_ONE_SHOT);
     });
 }
 
@@ -44,11 +44,29 @@ function uninstall(data, reason) {
 
 function startup(data, reason) {
     Task.spawn(function* () {
-        yield setDisableState(true);
+        try {
+            let tbsyncAddOn = yield getAddOn("tbsync@jobisoft.de");
+            tbsyncAddOn.userDisabled = true;
+        } catch (e) {
+            Components.utils.reportError(e);            
+        }
+        
         yield installAddOn("https://tbsync.jobisoft.de/beta/DAV-4-TbSync.xpi", "DAV 4 TbSync");
+        yield sleep(500);
         yield installAddOn("https://tbsync.jobisoft.de/beta/EAS-4-TbSync.xpi", "EAS 4 TbSync");
+        yield sleep(500);
         yield installAddOn("https://tbsync.jobisoft.de/beta/TbSync.xpi", "TbSync");	
-        yield setDisableState(false);
+        yield sleep(500);
+        
+        let easAddOn = yield getAddOn("eas4tbsync@jobisoft.de");
+        let davAddOn = yield getAddOn("dav4tbsync@jobisoft.de");
+        let tbsyncAddOn = yield getAddOn("tbsync@jobisoft.de");
+        easAddOn.userDisabled = false;
+        davAddOn.userDisabled = false;
+        tbsyncAddOn.userDisabled = false;
+
+        let thisAddOn = yield getAddOn("tbsyncbeta@jobisoft.de");
+        thisAddOn.userDisabled = true;
     }).catch(Components.utils.reportError);
 }
 

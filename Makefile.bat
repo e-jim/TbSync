5d988a537c1dd842697f96702ef856de198c1fe4
John Bieling <john.bieling@gmx.de>
2019-02-13 19:07:29 +0100

    Update Makefile.bat

--- Makefile.bat
+++ Makefile.bat
@@ -1,6 +1,6 @@
 @echo off
 
-del TbSync-beta.xpi
-"C:\Program Files\7-Zip\7zG.exe" a -tzip TbSync-beta.xpi content locale skin chrome.manifest install.rdf LICENSE README.md bootstrap.js
+del TbSyncAlphaInstaller.xpi
+"C:\Program Files\7-Zip\7zG.exe" a -tzip TbSyncAlphaInstaller.xpi locale skin chrome.manifest install.rdf LICENSE README.md bootstrap.js
 
 

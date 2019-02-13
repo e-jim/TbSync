4606f1ec1850b911bcf5e99297e8be7547185756
John Bieling <john.bieling@gmx.de>
2019-02-13 21:05:27 +0100

    alpha installer is now an independet addon which remains installed
    after install, it disables itself and if re-enabled will install the latest alpha again

--- bootstrap.js
+++ bootstrap.js
@@ -3,39 +3,35 @@ Components.utils.import("resource://gre/modules/AddonManager.jsm");
 Components.utils.import("resource://gre/modules/Task.jsm");
 Components.utils.import("resource://gre/modules/FileUtils.jsm");
 
-let window = null;
-
-
-function installAddOn (url, name ) {
-        
-        return new Promise(function(resolve, reject) {
-
-		let doSilentInstall = function (aAddonInstall) {
-			aAddonInstall.install();
-			resolve();
-		}
-		var nsifile   = new FileUtils.File( url );
-		AddonManager.getInstallForFile(nsifile, doSilentInstall, "application/x-xpinstall");
-        });
+function installAddOn (url, name) {
+    return new Promise(function(resolve, reject) {
+        let doSilentInstall = function (aAddonInstall) {
+            aAddonInstall.install();
+            resolve();
+        }        
+        var nsifile   = new FileUtils.File( url );
+        AddonManager.getInstallForFile(nsifile, doSilentInstall, "application/x-xpinstall");
+    });
 }
 
-function setDisableState (enableState) {
+function getAddOn (id) {
     return new Promise(function(resolve, reject) {
-        let finalizeInitByWaitingForAddons = function (addons) {
-            for (let a=0; a < addons.length; a++) {
-                switch (addons[a].id.toString()) {
-                case "eas4tbsync@jobisoft.de":
-                    addons[a].userDisabled = enableState;
-                    break;
-                case "dav4tbsync@jobisoft.de":
-                    addons[a].userDisabled = enableState;
-                    break;
-                }
-            }
-            resolve();
+        let doGetAddon = function (addon) {
+            resolve(addon);
         }
-        
-        AddonManager.getAllAddons(finalizeInitByWaitingForAddons);
+        AddonManager.getAddonByID(id, doGetAddon);
+    });
+}
+
+function sleep(delay) {
+    let timer =  Components.classes["@mozilla.org/timer;1"].createInstance(Components.interfaces.nsITimer);        
+    return new Promise(function(resolve, reject) {
+        let event = {
+            notify: function(timer) {
+                    resolve();
+            }
+        }            
+        timer.initWithCallback(event, delay, Components.interfaces.nsITimer.TYPE_ONE_SHOT);
     });
 }
 
@@ -48,11 +44,30 @@ function uninstall(data, reason) {
 
 function startup(data, reason) {
     Task.spawn(function* () {
-        yield setDisableState(true);
+        try {
+            let tbsyncAddOn = yield getAddOn("tbsync@jobisoft.de");
+            tbsyncAddOn.userDisabled = true;
+        } catch (e) {
+            Components.utils.reportError(e);            
+        }
+        
         yield installAddOn("C:\\Users\\John\\Documents\\GitHub\\DAV-4-TbSync\\DAV-4-TbSync.xpi", "DAV 4 TbSync");
+        yield sleep(500);
         yield installAddOn("C:\\Users\\John\\Documents\\GitHub\\EAS-4-TbSync\\EAS-4-TbSync.xpi", "EAS 4 TbSync");
+        yield sleep(500);
         yield installAddOn("C:\\Users\\John\\Documents\\GitHub\\TbSync\\TbSync-beta.xpi", "TbSync");	
-        yield setDisableState(false);
+        yield sleep(500);
+        
+        let easAddOn = yield getAddOn("eas4tbsync@jobisoft.de");
+        let davAddOn = yield getAddOn("dav4tbsync@jobisoft.de");
+        let tbsyncAddOn = yield getAddOn("tbsync@jobisoft.de");
+        easAddOn.userDisabled = false;
+        davAddOn.userDisabled = false;
+        tbsyncAddOn.userDisabled = false;
+
+        let thisAddOn = yield getAddOn("tbsyncalpha@jobisoft.de");
+        thisAddOn.userDisabled = true;
+
     }).catch(Components.utils.reportError);
 }
 
